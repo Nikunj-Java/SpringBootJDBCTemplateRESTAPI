206bb8d1e7e293930c4ee0cb314c27ce4fdcec2e
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-07-31 16:33:56 +0530

    Nginx

--- src/main/java/com/neueda/restapi/EmployeeController.java
+++ src/main/java/com/neueda/restapi/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 //
 @RestController
 @RequestMapping("/api/v1/employee")
-@CrossOrigin(origins = "http://10.9.71.48:5500")
+@CrossOrigin(origins = "http://10.9.71.48:8085")
 public class EmployeeController {
 
 
